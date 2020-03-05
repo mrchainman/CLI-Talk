eb1a182d73705243d6c015e4d37fdf3d970639e4
Mr Chainman <mr.chainman@0box.eu>
2020-03-05 20:53:21 +0100

    update

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ if __name__ == "__main__":
     # TODO: This nested while loop is ugly, find a better way to handle it
     while True:
         list_conversations()
-        conversation = input("Please enter the name of a conversation (type 'quit' to exit) : ")
-        if conversation == "quit":
+        conversation = input("Please enter the name of a conversation (type 'q' to exit) : ")
+        if conversation == "q":
             print("Exiting ...")
             break
         while True:
             print("Loading messages ...")
             get_messages(conversation)
-            msg = input("Send message (type 'quit' to exit, 'load' to reload the messages) : ")
-            if msg == "quit":
+            msg = input("Send message (type 'q' to go back, 'l' to reload the messages) : ")
+            if msg == "q":
                 break
-            elif msg == "load":
+            elif msg == "l":
                 continue
             else:
                 send_msg(conversation, msg)
