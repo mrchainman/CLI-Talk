3c3f745d465f2f68967526d38c10b9a652ee2efc
Mr Chainman <mr.chainman@0box.eu>
2020-03-05 21:12:32 +0100

    Updated comments in tests.py

--- tests.py
+++ tests.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 #TODO Write some tests
+# The Problem is that without a running Nextcloud instance writing tests is difficult
+# and I don't want to put the credentials for my instance into the code...
+# If somebody has some ideas, just make a PR
 def test_fake():
     assert 1 == 1
