2c363d0e7dc6b0d7071415eb97ff742b085316d6
Mr Chainman <mr.chainman@0box.eu>
2020-03-06 19:52:59 +0100

    Added some tests

---
+++ cache/README.md
@@ -0,0 +1,2 @@
+# Cachedir
+This file only exists to add the cache dir to the git repo
--- tests/tests.py
+++ tests/tests.py
@@ -3,5 +3,20 @@
 # The Problem is that without a running Nextcloud instance writing tests is difficult
 # and I don't want to put the credentials for my instance into the code...
 # If somebody has some ideas, just make a PR
-def test_fake():
-    assert 1 == 1
+import sys
+sys.path.append('../bin')
+from main import *
+
+# Currently breaks other tests, as direct import is not possible
+# def test_create_config():
+#     try:
+#         import config
+#     except:
+#         with open ("../bin/config.py",'a') as f:
+#             f.write(f"#!/usr/bin/python3\nurl = \"https://cloud.example.com/ocs/v2.php/apps/spreed/api/v1\"\nuser = \"johndoe\"\npassword = \"s3cr3t\"")
+
+def test_import_config():
+    import config
+    assert len(url) != 0
+    assert len(user) != 0
+    assert len(password) != 0
